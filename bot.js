8c8e579d1bcbbe6e8e96f0b764cab999306b277b
Jack Price <32805567+Ferexx@users.noreply.github.com>
2019-07-31 23:46:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,26 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const auth = require('./auth.json');
-const http = new XMLHttpRequest();
-const twitchData = {
-    
-}
+/*const twitchData = {
+    hub.callback = "";
+    hub.mode = "subscribe";
+    hub.topic = "https://api.twitch.tv/helix/streams?user_id=52993629";
+    hub.lease_seconds = 864000;
+}*/
+//var xhr = new XMLHttpRequest();
 var csgoScheduleURL = '';
 var leagueScheduleURL = '';
-
-
-//Initialisation for bot to connect to Twitch
-const clientId = '???'
-export default clientId;
-const axios = require('axios')
-import clientId from '../secrets'
-axios.defaults.headers.common['Client-ID'] = clientId
 
 bot.login(auth.token);
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);
+    //subscribeToTwitch();
 });
 
 bot.on('message', msg => {
     var text = msg.content;
+    text = text.toLowerCase();
     if(text.substring(0, 1) === '!') {
         if (text === '!ping') {
             msg.reply('pong');
@@ -41,20 +38,53 @@ bot.on('message', msg => {
         if (text.includes('set') && msg.member.roles.find(r => r.name ==='Administrator')) {
             setGameInfo(text.split(" "));
         }
+
+        //Adding roles to people as they request them
+        if(msg.channel.name==='roles') {
+            role = text.substring(1, text.length);
+
+            //Games
+            switch(role) {
+                case 'csgo':
+                    msg.member.addRole('584072334537916431');
+                    break;
+                case 'league':
+                    msg.member.addRole('584072216535367691');
+                    break;
+                case 'rocket':
+                    msg.member.addRole('584072288962347019');
+                    break;
+                case 'siege':
+                    msg.member.addRole('584072411423571992')
+                    break;
+                case 'smash':
+                    msg.member.addRole('584072461507887107');
+                    break;
+                case 'overwatch':
+                    msg.member.addRole('584090694453428274');
+                    break;
+                case 'tft':
+                    msg.member.addRole('606254744314576896');
+                    break;
+
+                //Colleges
+                default:
+                    msg.member.addRole(msg.guild.roles.findKey(r => r.name.toLowerCase() === role.toLowerCase()));
+            }
+        }
     }
 });
 
 
-async componentDidMount(){
-    let {info} = await axios.get('https://api.twitch.tv/helix/streams?user_id=52993629')
-    console.log(info)
+function subscribeToTwitch() {
+    xhr.open("POST", "https://api.twitch.tv/helix/webhooks/hub", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(twitchData));
+    xhr.onreadystatechange = function() {
+        var json = JSON.parse(xhr.responseText);
+    }
 }
 
-http.open("POST", "https://api.twitch.tv/helix/webhooks/hub");
-http.send();
-http.onreadystatechange=(e)=> {
-
-}
 
 
 function setGameInfo(array) {
